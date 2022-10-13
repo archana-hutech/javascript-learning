e33869691a621c8fbf7540c1c48149c137a98b02
archana-hutech <archana@hutechsolutions.com>
2022-10-13 18:02:42 +0530

    13/10/22
    const, operators, arithmetic, assignment, precedence, datatype

--- js-basics/test.js
+++ js-basics/test.js
@@ -61,4 +61,57 @@ van = "Volvo";
 var van;
 
 _x = "xyz";
-//let _x;
+//let _x;  not possible
+let hjk =  "yuy"; 
+console.log(hjk);
+
+//JS const
+//------------------
+//reassigning the variable
+const f = 2;     // Allowed
+//x = 2; not allowed
+//creating an constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+// can change an element of an array:
+cars[0] = "Toyota";
+//can add an element to the array:
+cars.push("Audi");
+console.log(cars);
+//can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+// can change a property:
+car.color = "red";
+// can add a property:
+car.owner = "Johnson";
+//Const hoisting
+const carName1 = "Volvo";
+console.log(carName1);
+//assignment operator
+let ass = 10;
+ass += 5;
+console.log(ass);
+//adding strings
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+console.log(text3);
+//arithmetic expression
+let a = 2;
+let s = (100 + 50) * a;
+console.log(s);
+let q = 16 + 4 + "Volvo";
+console.log(q);
+//JS as dynamic datatype
+let p;           // Now x is undefined
+p = 5;           // Now x is a Number
+p = "John";      // Now x is a String
+console.log(p);
+let carName2 = "Volvo XC60";   // Using double quotes
+let carName3 = 'Volvo XC60';   // Using single quotes
+let x1 = 34.00;     // Written with decimals
+let x2 = 34;        // Written without decimals
+typeof ""             // Returns "string"
+typeof "John"         // Returns "string"
+typeof "John Doe"     // Returns "string"
+
+
