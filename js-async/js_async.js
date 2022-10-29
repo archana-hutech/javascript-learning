5800db1b19b23ac36cd88fb19f76607bea283691
archana-hutech <archana@hutechsolutions.com>
2022-10-29 12:16:56 +0530

    29/10/22-1
    async/await

--- js-async/js_async.js
+++ js-async/js_async.js
@@ -142,3 +142,21 @@ async function myplay() {
     console.log(await myPro);
 }
 myplay();
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+}
+
+asyncCall();
+
